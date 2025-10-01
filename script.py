77c1bbff7a939dd27e8d674ced2b53abf81a007b
Max <maximus200674@gmail.com>
2025-10-01 17:01:56 +0700

    2.1.1

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import json, shlex      # shlex разделяет строку на части, учитывая кавычки
+import json, shlex
 
 filename = "input.txt"
 with open(filename, encoding="utf-8") as f:
@@ -7,9 +7,7 @@ with open(filename, encoding="utf-8") as f:
         if not line:
             continue
         type_part, props = line.split(":", 1)
-        tokens = shlex.split(props)         # корректное разбиение props
+        tokens = shlex.split(props)
         date, plate, mark, speed = tokens[0], tokens[1], tokens[2], tokens[3]
         obj = {"type": type_part, "date": date, "plate": plate, "mark": mark, "speed": speed}
-        print(json.dumps(obj, ensure_ascii=False))          #преобразуем словарь в json-строку
-
-
+        print(json.dumps(obj, ensure_ascii=False))
